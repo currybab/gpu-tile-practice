d19167649525cd815295ed56331ad57b6217aa3a
Jun Park <pj2417@gmail.com>
2026-01-29 23:47:35 +0900

    cute-dsl: Layout 인덱스 매핑 예제 및 사용법 추가

--- cute-dsl/13-layout.py
+++ cute-dsl/13-layout.py
@@ -2,6 +2,20 @@ import cutlass
 import cutlass.cute as cute
 
 @cute.jit
+def print2D(layout: cute.Layout):
+    # Python DSL에서는 layout(m, n)이 아니라 layout((m, n)) 형태로 호출해야 함.
+    for m in cutlass.range_constexpr(cute.size(layout, mode=[0])):
+        for n in cutlass.range_constexpr(cute.size(layout, mode=[1])):
+            print(layout((m, n)), end="  ")
+        print("")
+
+@cute.jit
+def print1D(layout: cute.Layout):
+    for m in cutlass.range_constexpr(cute.size(layout)):
+        print(layout(m), end="  ")
+    print("")        
+
+@cute.jit
 def layout_test():
     t = ((1,2), (3,4))
     cute.printf("t.rank={}, t.depth={}, t.size={}, t.get(0)={}, t.get(1)={}", cute.rank(t), cute.depth(t), cute.size(t), cute.get(t, (0,)), cute.get(t, (1,)))
@@ -72,11 +86,41 @@ def layout_test():
     cute.printf("dynamic print s2xh4_col: {}", s2xh4_col)
     cute.printf("dynamic print s2xh4_row: {}", s2xh4_row)
 
+
     """
     print 결과:
     dynamic print s2xh4_col: (2,(2,2)):(1,(2,4))
     dynamic print s2xh4_row: (2,(2,2)):(4,(2,1))
     """
+
+    #####################################################
+
     cute.printf("congruent: {}", cute.is_congruent(s2xh4_row.shape, s2xh4_col.stride))
+    
+    #####################################################
+    
+    print("s2xs4 2D index map:") 
+    print2D(s2xs4)
+
+    print("s2xh4_col 2D index map:") # (2,(2,2)):(1,(2,4))
+    print2D(s2xh4_col)
+
+    print("s2xh4_row 2D index map:") # (2,(2,2)):(4,(2,1))
+    print2D(s2xh4_row)
+
+    #####################################################
+
+    print("s2xs4 1D index map:") 
+    print1D(s2xs4)
+
+    print("s2xh4_col 1D index map:") # (2,(2,2)):(1,(2,4))
+    print1D(s2xh4_col)
+
+    print("s2xh4_row 1D index map:") # (2,(2,2)):(4,(2,1))
+    print1D(s2xh4_row)
+
+    #######################################################
+
+
 
 layout_test()
